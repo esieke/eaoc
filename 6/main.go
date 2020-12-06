8c6617ca03c1f4c17ff4dd9178ff3748120407f9
Elmar Sieke <elmar.sieke@gmx.net>
2020-12-06 16:40:10 +0100

    day 6 puzzle 2

--- 6/main.go
+++ 6/main.go
@@ -15,29 +15,34 @@ func main() {
 	defer input.Close()
 	s := bufio.NewScanner(input)
 
-	var group map[byte]bool
+	var group map[byte]int
 	result := 0
-	group = make(map[byte]bool)
+	group = make(map[byte]int)
 	for s.Scan() {
 		l := []byte(s.Text())
 		if len(l) == 0 {
-			for _, y := range group {
-				if y {
-					result++
+			for k, y := range group {
+				if k != '!' {
+					if group['!'] == y {
+						result++
+					}
 				}
 			}
-			group = make(map[byte]bool)
+			group = make(map[byte]int)
 			continue
 		}
+		group['!']++
 		for _, b := range l {
-			group[b] = true
+			group[b]++
 		}
 
 	}
 	// last group
-	for _, y := range group {
-		if y {
-			result++
+	for k, y := range group {
+		if k != '!' {
+			if group['!'] == y {
+				result++
+			}
 		}
 	}
 
