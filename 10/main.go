a3df6c199abd229f5b99e42d5240648a9d106294
Elmar Sieke <elmar.sieke@gmx.net>
2020-12-10 19:36:47 +0100

    day 10 puzzle 2

--- 10/main.go
+++ 10/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -32,12 +33,43 @@ func main() {
 	sort.Ints(joltAdapt)
 	joltAdapt = append(joltAdapt, joltAdapt[len(joltAdapt)-1]+3)
 
-	diffCtr := make(map[int]int)
+	parts := make([][]int, 0, 1024)
+	var last int
 	for i := range joltAdapt {
 		if i == 0 {
 			continue
 		}
-		diffCtr[joltAdapt[i]-joltAdapt[i-1]]++
+		if joltAdapt[i]-joltAdapt[i-1] == 3 {
+			parts = append(parts, joltAdapt[last:i])
+			last = i
+		}
 	}
-	fmt.Println(diffCtr[1] * diffCtr[3])
+
+	result := 1
+	for _, joltAdapGroup := range parts {
+		if len(joltAdapGroup) <= 2 {
+			continue
+		}
+		partResult := 0
+		solutions := int(math.Pow(2.0, float64(len(joltAdapGroup)-2)))
+		for bitMask := 0; bitMask < solutions; bitMask++ {
+			last = joltAdapGroup[0]
+			invalid := false
+			for i := 1; i < len(joltAdapGroup); i++ {
+				if 1<<(i-1)&bitMask != 0 || i == len(joltAdapGroup)-1 {
+					if joltAdapGroup[i]-last > 3 {
+						invalid = true
+						break
+					}
+					last = joltAdapGroup[i]
+				}
+			}
+			if invalid {
+				continue
+			}
+			partResult++
+		}
+		result *= partResult
+	}
+	fmt.Println(result)
 }
