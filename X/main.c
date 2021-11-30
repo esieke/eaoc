d74df52431c8aae76b699000eb16bcd2e265ed6a
Elmar Sieke <elmar.sieke@gmx.net>
2021-11-30 23:54:48 +0100

    init for 2021

---
+++ X/main.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "errno.h"
+
+#define STR_LEN 500
+
+int aocOpen(const char *name, FILE **f)
+{
+	int lines;
+	char buf;
+
+	*f = fopen(name, "r");
+	if (*f == NULL)
+		return -1;
+
+	while (1)
+	{
+		int r = fscanf(*f, "%c", &buf);
+		if (r == EOF && errno > 0)
+			return -2;
+		if (buf == '\n' || r == EOF)
+			lines++;
+		if (r == EOF)
+			break;
+	}
+
+	if (fseek(*f, 0, SEEK_SET) < 0)
+		return -2;
+
+	return lines;
+}
+
+
+
