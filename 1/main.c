f5e123308b89d6cd8cee1f54a4d1eb8f2d97b810
Elmar Sieke <elmar.sieke@gmx.net>
2021-12-01 08:44:31 +0100

    Day 1 puzzle 1

---
+++ 1/main.c
@@ -0,0 +1,59 @@
+#include "stdio.h"
+#include "errno.h"
+
+#define STR_LEN 500
+
+int aocOpen(const char *name, FILE **f)
+{
+	int lines;
+	char buf;
+
+	*f = fopen(name, "r");
+	if (*f == NULL)
+		return -1;
+
+	while (1)
+	{
+		int r = fscanf(*f, "%c", &buf);
+		if (r == EOF && errno > 0)
+			return -2;
+		if (buf == '\n' || r == EOF)
+			lines++;
+		if (r == EOF)
+			break;
+	}
+
+	if (fseek(*f, 0, SEEK_SET) < 0)
+		return -2;
+
+	return lines;
+}
+
+int main()
+{
+	FILE *f;
+	int l = aocOpen("../../1/input", &f);
+	if (l < 0)
+	{
+		printf("open input file failed with error\n");
+		return 1;
+	}
+
+	int count = 0;
+	int in[l];
+	for (int i = 1; i < l; i++)
+	{
+		int r = fscanf(f, "%d", &in[i]);
+		if (r == EOF && errno > 0)
+			return -2;
+	}
+
+	for (int i = 1; i < l; i++)
+	{
+		if (in[i] > in[i - 1])
+			count++;
+	}
+	printf("%d\n", count);
+
+	return 0;
+}
