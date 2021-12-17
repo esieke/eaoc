a71b1253d716393ed9be21e13df36a101ef3b770
Elmar Sieke <elmar.sieke@gmx.net>
2021-12-17 22:54:12 +0100

    Day 17 puzzle 2

--- 17/main.c
+++ 17/main.c
@@ -16,6 +16,7 @@ typedef struct pos_
 
 typedef struct target_
 {
+	int ctr;
 	pos b;
 	pos e;
 } target;
@@ -69,35 +70,28 @@ int simu(target *t)
 {
 	int yMx = 0;
 
-	for (int xi = 1; xi < t->e.x; xi++)
+	for (int xi = 1; xi < t->e.x + 10; xi++)
 	{
 		probe p = {.p.x = 0, .p.y = t->b.y, .v.y = 0, .v.x = xi, .mx.y = 0, .mx.x = 0};
-		while (1)
 		{
-			step(&p);
-			p.p.y = t->b.y;
-			int cfh = checkForHit(&p, t);
-			if (cfh > 0)
+			for (int yi = t->e.y; yi < 2 * (-1 * t->e.y); yi++)
 			{
-				for (int yi = 1; yi < (-1*t->e.y +1); yi++)
+				probe p2 = {.p.x = 0, .p.y = 0, .v.y = yi, .v.x = xi, .mx.y = 0, .mx.x = 0};
+				while (1)
 				{
-					probe p2 = {.p.x = 0, .p.y = 0, .v.y = yi, .v.x = xi, .mx.y = 0, .mx.x = 0};
-					while (1)
+					step(&p2);
+					int cfh2 = checkForHit(&p2, t);
+					if (cfh2 > 0)
 					{
-						step(&p2);
-						int cfh2 = checkForHit(&p2, t);
-						if (cfh2 > 0)
-						{
-							if (p2.mx.y > yMx)
-								yMx = p2.mx.y;
-						}
-						if (cfh2 < 0)
-							break;
+						t->ctr++;
+						if (p2.mx.y > yMx)
+							yMx = p2.mx.y;
+						break;
 					}
+					if (cfh2 < 0)
+						break;
 				}
 			}
-			if (cfh < 0 || vx(&p) <= 0)
-				break;
 		}
 	}
 	return yMx;
@@ -106,11 +100,12 @@ int simu(target *t)
 int main()
 {
 	// target area: x=20..30, y=-10..-5
-	// target t = {.b = {.y = -5, .x = 20}, .e = {.y = -10, .x = 30}};
+	// target t = {.b = {.y = -5, .x = 20}, .e = {.y = -10, .x = 30}, .ctr = 0};
 	// target area: x=94..151, y=-156..-103
-	target t = {.b = {.y = -103, .x = 94},.e = {.y = -156, .x = 151}};
+	target t = {.b = {.y = -103, .x = 94},.e = {.y = -156, .x = 151}, .ctr = 0};
 
 	int res = simu(&t);
 	printf("%d\n", res);
+	printf("%d\n", t.ctr);
 	return 0;
 }
