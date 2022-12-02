4b90989fd6b7563991cad758de8fb069ccf711cb
Elmar Sieke <elmar.sieke@gmx.net>
2022-12-02 15:23:54 +0100

    day 2 puzzle 2

--- 2/main.go
+++ 2/main.go
@@ -18,7 +18,7 @@ func main() {
 	totalScore := 0
 	for s.Scan() {
 		l := s.Text()
-		totalScore += getScore(l)
+		totalScore += getScore2(l)
 	}
 	fmt.Println(totalScore)
 }
@@ -45,3 +45,27 @@ func getScore(game string) int {
 // X lose
 // Y draw
 // Z win
+func getScore2(game string) int {
+	// Rock, Paper, Scissors
+	// 'A' == 65
+	// 'X' == 88
+	b := []byte(game)
+	if b[2]-87 == 1 {
+		// lose
+		my := int(b[0] - 64 - 1)
+		if my < 1 {
+			my = 3
+		}
+		return my + 0
+	}
+	if b[2]-87 == 2 {
+		// draw
+		return int(b[0] - 64 + 3)
+	}
+	// win
+	my := int(b[0] - 64 + 1)
+	if my > 3 {
+		my = 1
+	}
+	return my + 6
+}
