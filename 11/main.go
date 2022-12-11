0cf3f3c43693e3489186bd4e88202ffdf8471f60
Elmar Sieke <elmar.sieke@gmx.net>
2022-12-11 20:26:21 +0100

    day 11 puzzle 2

--- 11/main.go
+++ 11/main.go
@@ -10,12 +10,13 @@ func main() {
 	ms := puzzle()
 
 	ins := make([]int, len(ms))
-	for round := 0; round < 20; round++ {
+	for round := 0; round < 10000; round++ {
 		for mi, m := range ms {
 			for _, i := range m.items {
 				ins[mi] += 1
 				w := m.operation(i)
-				w = w / 3
+				//w = w % (17 * 13 * 19 * 23) // example
+				w = w % (2 * 7 * 3 * 17 * 11 * 19 * 5 * 13) // puzzle
 				throwTo := m.test(w)
 				ms[throwTo].items = append(ms[throwTo].items, w)
 			}
